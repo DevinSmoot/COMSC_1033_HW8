ef8d0b5f09966fd0bccef34c521e1a6a43cbaa97
Devin Smoot <smootd@student.swosu.edu>
2015-11-06 09:24:45 -0600

    Adjusted code for more efficiency

--- src/COMSC_1033_HW8.java
+++ src/COMSC_1033_HW8.java
@@ -5,19 +5,22 @@
  */
 public class COMSC_1033_HW8 {
 	public static void main(String[] args) {
-		int randomNum;
+		
+		char letter = randomLetter();
+		System.out.println(letter);
+	}
+	public static char randomLetter () {
+		char result; // This will hold our randomly generated number for return.
 		// Set integers to hold number range
+		
 		int startingValue = 65; // 'A' begins at 65
 		int endingValue = 90 - startingValue + 1; // 'Z' is 90
 		//System.out.println(startingValue); //Debug statement: View startingValue
 		//System.out.println(endingValue); //Debug statement: View endingValue
 		//randomNum (startingValue ,endingValue); //Debug statement: Call randomChar method
-		char letter = (char)randomNum(startingValue,endingValue);
-		System.out.println(letter);
-	}
-	public static int randomNum (int startNum, int endNum) {
-		int result; // This will hold our randomly generated number for return.
-		result = (int)(startNum + Math.random()*endNum);
+		//char letter = (char)randomNum(startingValue,endingValue);
+		result = (char)(startingValue + Math.random()*endingValue);
+		
 		//System.out.println(randomNumber); //Debug statement: View randomNumber
 		return result;
 	}
